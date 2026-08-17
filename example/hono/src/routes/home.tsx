83c4d5de9c598f6611e4eb1e9481495bd6579d23
mufeng <mufeng.me@gmail.com>
2026-08-17 10:11:11 +0800

    feat(examples): add interactive counters

--- example/hono/src/routes/home.tsx
+++ example/hono/src/routes/home.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
+
 export default function Home() {
+  const [count, setCount] = useState(1)
+
   return (
     <div>
       <h1>@stormory/react-router-server</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(p => p + 1)}>Increase</button>
+      <button onClick={() => setCount(p => p - 1)}>Decrease</button>
     </div>
   )
 }
